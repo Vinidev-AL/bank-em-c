eaab5c4fd0f4f5ed71d12ce551066a1fdd8302ae
ARTHUR GABRIEL ALMEIDA DE MOURA <arthur.gmoura@a.ucb.br>
2024-05-23 11:11:31 -0300

    feito structs Cliente

--- structs.cpp
+++ structs.cpp
@@ -6,4 +6,34 @@ typedef struct Data{
 	int dia,
 		mes,
 		ano;
-} Data;hjhjh
+} Data;
+
+typedef struct Endereco{
+	char logradouro[50];
+	char n_casa[20];
+	char bairro[20];
+	char cidade[20];
+	char estado[20];
+
+}Endereco;
+
+typedef struct Telefone{
+	int DDD;
+	int numero;
+	
+}Telefone;
+
+typedef struct Nome{
+	int Nome,
+	Sobrenome;
+}Nome;
+
+typedef struct Cliente{
+	Data data;
+	char cpf;
+	Endereco endereco;
+	Telefone telefone;
+	Nome nome;
+}Cliente;
+
+                                                                                                             
